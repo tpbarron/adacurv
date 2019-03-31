75e588146bf143ab1ea685638916e7a3aa28f4f4
Trevor Barron <barron.trevor@gmail.com>
2019-03-31 13:03:37 -0700

    adding multiple options for shrinkage

--- adacurv/torch/utils/lanczos.py
+++ adacurv/torch/utils/lanczos.py
@@ -55,58 +55,70 @@ def lanczos_iteration(Fvp_fn, dim, k=20):
     w = eigvalsh_tridiagonal(np.array(diag), np.array(diag_adj))
     return w
 
-def estimate_shrinkage_buggy(eigvals, p, batch_size):
-    # p = len(eigvals)
-    # Tr(s) = Sum(\lambda_i)
-    trS = np.sum(eigvals)
-    # print ("trS: ", trS)
 
-    # Tr^2(S) - computed knowing that Tr(s) = Sum(eigvals(S))
-    tr2S = trS**2.0
-    # print ("tr2S: ", tr2S)
-
-    # TODO: optimize this.
-    coef = 0.0
-    for j in range(len(eigvals)):
-        for i in range(j):
-            coef += eigvals[j] * eigvals[i]
-    trS2 = tr2S - 2.0 * coef
-    # print ("trS2: ", trS2)
-
-    # print ("numer: ", ((1.0 - 2.0 / p) * trS2 + tr2S))
-    # print ("denom: ", ((batch_size + 1 - 2.0 / p) * (trS2 - tr2S / p)) )
-    rho = min(((1.0 - 2.0 / p) * trS2 + tr2S) / ((batch_size + 1 - 2.0 / p) * (trS2 - tr2S / p)), 1.0)
-    diag_shrunk = trS / p
-
-    return float(rho), float(diag_shrunk)
+USE_ORIGINAL_SHRINKAGE = True
 
 
-def estimate_shrinkage(eigvals, p, batch_size):
-    p = len(eigvals)
-    # Tr(s) = Sum(\lambda_i)
-    trS = np.sum(eigvals)
-    # print ("trS: ", trS)
+if USE_ORIGINAL_SHRINKAGE:
+    # This uses the expression from https://arxiv.org/pdf/0907.4698.pdf
+    def estimate_shrinkage(eigvals, p, batch_size):
+        # p = len(eigvals)
+        # Tr(s) = Sum(\lambda_i)
+        trS = np.sum(eigvals)
+        # print ("trS: ", trS)
 
-    # Tr^2(S) - computed knowing that Tr(s) = Sum(eigvals(S))
-    tr2S = trS**2.0
-    # print ("tr2S: ", tr2S)
+        # Tr^2(S) - computed knowing that Tr(s) = Sum(eigvals(S))
+        tr2S = trS**2.0
+        # print ("tr2S: ", tr2S)
 
-    # # TODO: optimize this.
-    # coef = 0.0
-    # for j in range(len(eigvals)):
-    #     for i in range(j):
-    #         coef += eigvals[j] * eigvals[i]
-    # trS2 = tr2S - 2.0 * coef
-    trS2 = np.sum(eigvals**2.0)
-    # print ("trS2: ", trS2)
+        # TODO: optimize this.
+        # coef = 0.0
+        # for j in range(len(eigvals)):
+        #     for i in range(j):
+        #         coef += eigvals[j] * eigvals[i]
+        # trS21 = tr2S - 2.0 * coef
+        trS2 = np.sum(eigvals**2.0)
+        # print ("trS2: ", trS21, trS2)
 
-    # print ("numer: ", ((1.0 - 2.0 / p) * trS2 + tr2S))
-    # print ("denom: ", ((batch_size + 1 - 2.0 / p) * (trS2 - tr2S / p)) )
+        numer = (1.0 - 2.0 / p) * trS2 + tr2S
+        denom = (batch_size + 1 - 2.0 / p) * (trS2 - tr2S / p)
+        # print ("numer: ", ((1.0 - 2.0 / p) * trS2 + tr2S))
+        # print ("denom: ", ((batch_size + 1 - 2.0 / p) * (trS2 - tr2S / p)) )
+        rho = min(numer / denom, 1.0)
+        diag_shrunk = trS / p
 
-    numer = ((1.0 - 2.0) / p) * trS2 + tr2S
-    denom = ((batch_size + 1.0 - 2.0) / p) * (trS2 - (tr2S / p))
-    # print (numer, denom, numer / denom)
-    rho = min( numer / denom , 1.0)
-    diag_shrunk = trS / p
+        # print ("rho, D: ", rho, diag_shrunk)
+        return float(rho), float(diag_shrunk)
 
-    return float(rho), float(diag_shrunk)
+else:
+    # This uses the expression from
+    # http://webee.technion.ac.il/Sites/People/YoninaEldar/Info/Shrink.pdf
+    # and does not appear to be as effective empirically.
+    def estimate_shrinkage(eigvals, p, batch_size):
+        # p = len(eigvals)
+        # Tr(s) = Sum(\lambda_i)
+        trS = np.sum(eigvals)
+        # print ("trS: ", trS)
+
+        # Tr^2(S) - computed knowing that Tr(s) = Sum(eigvals(S))
+        tr2S = trS**2.0
+        # print ("tr2S: ", tr2S)
+
+        # TODO: optimize this.
+        # coef = 0.0
+        # for j in range(len(eigvals)):
+        #     for i in range(j):
+        #         coef += eigvals[j] * eigvals[i]
+        # trS21 = tr2S - 2.0 * coef
+        trS2 = np.sum(eigvals**2.0)
+        # print ("trS2: ", trS21, trS2)
+
+        # print ("numer: ", ((1.0 - 2.0 / p) * trS2 + tr2S))
+        # print ("denom: ", ((batch_size + 1 - 2.0 / p) * (trS2 - tr2S / p)) )
+        numer = ((1.0 - 2.0) / p) * trS2 + tr2S
+        denom = ((batch_size + 1.0 - 2.0) / p) * (trS2 - (tr2S / p))
+        # print (numer, denom, numer / denom)
+        rho = min( numer / denom , 1.0)
+        diag_shrunk = trS / p
+
+        return float(rho), float(diag_shrunk)
