282e91578ca911f581137613cc1355f910d478c5
Trevor Barron <barron.trevor@gmail.com>
2019-04-16 23:01:02 -0700

    sync mat completion

---
+++ experiments/matrix_completion/climate_quick_start.py
@@ -0,0 +1,6 @@
+import climate_test
+
+
+if __name__ == "__main__":
+    climate_test.run(rcp='rcp45', iters=100)
+    climate_test.run(rcp='rcp85', iters=100)
--- experiments/matrix_completion/climate_test.py
+++ experiments/matrix_completion/climate_test.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
-import fisher.optim as fisher_optim
+import adacurv.torch.optim as fisher_optim
 from torch.nn.utils import vector_to_parameters, parameters_to_vector
 from torch.optim.lr_scheduler import LambdaLR
 
@@ -26,7 +26,7 @@ class Factorization(nn.Module):
             P = self.A @ self.B.t()
         return P
 
-def mat_completion_loss(W, M, P, A, B, ids, lmda1=0.01, lmda2=0.01):
+def mat_completion_loss(W, M, P, A, B, ids, lmda1=0.001, lmda2=0.001):
     # e = torch.norm(W[ids] * (M[ids] - P[ids]), 'fro') #** 2.0
     # print ("W: ", W.shape, W[ids].shape, P.shape)
     e = torch.norm(W[ids] * (M[ids] - P), 'fro') ** 2.0
@@ -36,6 +36,8 @@ def mat_completion_loss(W, M, P, A, B, ids, lmda1=0.01, lmda2=0.01):
 
 def build_mat_completion_loss_closure_combined(model, W, M, ids):
     def func(params):
+        # import time
+        # t1 = time.time()
         old_params = parameters_to_vector(model.parameters())
         if isinstance(params, Variable):
             vector_to_parameters(params, model.parameters())
@@ -47,40 +49,11 @@ def build_mat_completion_loss_closure_combined(model, W, M, ids):
 
         tmp_params = list(model.parameters())
         vector_to_parameters(old_params, model.parameters())
+        # t2 = time.time()
+        # print ("Loss closure time: ", (t2 - t1))
         return f, z, tmp_params
     return func
 
-
-def build_mat_completion_loss_closure(model, W, M, ids, mat='A'):
-    def func(params):
-        old_params = parameters_to_vector(model.parameters())
-
-        # print ("old: ", len(old_params), len(list(model.parameters())))
-        # print (type(params), type(params[0]))
-        if isinstance(params[0], torch.nn.Parameter):
-            # print ("1")
-            if mat == 'A':
-                vector_to_parameters(params[0].view(-1, 1), model.A) #parameters())
-            elif mat == 'B':
-                vector_to_parameters(params[0].view(-1, 1), model.B) #parameters())
-        else:
-            # print ("2")
-            if mat == 'A':
-                vector_to_parameters(parameters_to_vector(params[0]), model.A) #parameters())
-            elif mat == 'B':
-                vector_to_parameters(parameters_to_vector(params[0]), model.B) #parameters())
-
-        z = model(ids)
-        f = mat_completion_loss(W, M, z, model.A, model.B, ids)
-        if mat == 'A':
-            tmp_params = [model.A]
-        elif mat == 'B':
-            tmp_params = [model.B]
-
-        vector_to_parameters(old_params, model.parameters())
-        return f, z, tmp_params
-
-    return func
 
 def randomize_windices(W):
     Windx, Windy = W
@@ -89,8 +62,8 @@ def randomize_windices(W):
     Windy = Windy[perm]
     return Windx, Windy
 
-if __name__ == "__main__":
-    seed = 0
+
+def run(rcp='rcp45', iters=500, batch=2500, use_gn=True, seed=0):
     np.random.seed(seed)
     torch.manual_seed(seed)
 
@@ -99,13 +72,10 @@ if __name__ == "__main__":
     # B is (n x r)
     # So A @ B.T is (m x n)
 
-    rcp = 'rcp85'
     M = np.load("data/climate_data/matrices/M_1900_2101_"+rcp+".npy")
     W = np.load("data/climate_data/matrices/W_1900_2101_"+rcp+".npy")
-    # W[0:18,:] *= 0.1
-    # W[18:,:] *= 10.
 
-    bs = 5000
+    bs = batch
     n_samples = np.count_nonzero(W)
     print ("num sampes: ", n_samples)
     Wind = np.nonzero(W)
@@ -124,58 +94,35 @@ if __name__ == "__main__":
     print ("r, m, n: ", r, m, n)
     print ("A, B: ", fac.A.shape, fac.B.shape)
 
-    gn = True
-    if gn:
-        # optA = fisher_optim.NGD([fac.A, fac.B],
-        #                        lr=0.01,
-        #                        curv_type='gauss_newton',
-        #                        shrinkage_method=None, #'cg', #'lanzcos',
-        #                        lanczos_iters=0,
-        #                        batch_size=bs)
-        # optB = fisher_optim.NGD([fac.B],
-        #                        lr=0.001,
-        #                        curv_type='gauss_newton',
-        #                        shrinkage_method=None, #'cg', #'lanzcos',
-        #                        lanczos_iters=0,
-        #                        batch_size=bs)
+    if use_gn:
         optA = fisher_optim.NaturalAdam([fac.A, fac.B],
-                                         lr=0.01, #lr=0.01,
+                                         lr=0.01,
                                          curv_type='gauss_newton',
+                                         cg_prev_init_coef=0.0,
+                                         cg_precondition_empirical=False,
                                          shrinkage_method=None,
                                          batch_size=bs,
-                                         betas=(0.9, 0.9),
+                                         betas=(0.1, 0.1),
                                          assume_locally_linear=True)
-        # optB = fisher_optim.NaturalAdam([fac.B],
-        #                                  lr=0.001,
-        #                                  curv_type='gauss_newton',
-        #                                  shrinkage_method=None,
-        #                                  batch_size=bs,
-        #                                  betas=(0.9, 0.9),
-        #                                  assume_locally_linear=True)
     else:
         optA = optim.Adam([fac.A, fac.B], lr=0.01)
-        # optB = optim.Adam([fac.B], lr=0.001)
 
     P = fac(ids=Wind)
     print ("P init: ", P.shape)
     init_error = mat_completion_loss(W, M, P, fac.A, fac.B, Wind)
     print ("Init error: ", init_error / P.shape[0])
 
-    # lambda_lr = lambda epoch: 0.9 #1.0 / np.sqrt(epoch+1)
-    # scheduler = LambdaLR(opt, lr_lambda=[lambda_lr])
-
-    input("Start training?")
+    # input("Start training?")
     best_error = float(init_error)
 
     from torch.optim.lr_scheduler import ReduceLROnPlateau
     scheduler = ReduceLROnPlateau(optA, 'min')
 
-    for i in range(500):
+    for i in range(iters):
         Wind = randomize_windices(Wind)
         for j in range(n_batches):
 
             optA.zero_grad()
-            # optB.zero_grad()
 
             ind1 = j*bs
             ind2 = (j+1)*bs
@@ -188,15 +135,15 @@ if __name__ == "__main__":
             error = mat_completion_loss(W, M, P, fac.A, fac.B, batch_idx_idy)
             error.backward()
 
-            if gn:
+            if use_gn:
+                # import time
+                # t1 = time.time()
                 loss_closure = build_mat_completion_loss_closure_combined(fac, W, M, batch_idx_idy)
-                # loss_closure = build_mat_completion_loss_closure(fac, W, M, batch_idx_idy, mat='A')
+                # t2 = time.time()
+                # print ("Building loss closure time: ", (t2 - t1))
                 optA.step(loss_closure)
-                # loss_closure = build_mat_completion_loss_closure(fac, W, M, batch_idx_idy, mat='B')
-                # optB.step(loss_closure)
             else:
                 optA.step()
-                # optB.step()
 
             if j % 10 == 0:
                 print ("Iter: ", i, ", batch: ", j, float(error) / P.shape[0])
@@ -208,6 +155,12 @@ if __name__ == "__main__":
 
         if error < best_error:
             P2 = fac()
-            gn_str = 'gn' if gn else 'adam'
-            np.save('models/testlarge_P_'+rcp+'_rank'+str(r)+'_'+gn_str+'.npy', P2.data.numpy())
+            gn_str = 'gn' if use_gn else 'adam'
+            np.save('models/P_'+rcp+'_rank'+str(r)+'_'+gn_str+'.npy', P2.data.numpy())
             best_error = error
+
+
+
+if __name__ == "__main__":
+    run(rcp='rcp45')
+    run(rcp='rcp85')
